fe11675e4e692e5e20a1f80e3b2c113310aa0fd4
inkvizitor1991 <85788805+inkvizitor1991@users.noreply.github.com>
2021-10-26 18:19:29 +0300

    added the models

--- BakeCake/admin.py
+++ BakeCake/admin.py
@@ -1,15 +1,25 @@
 from django.contrib import admin
-from .forms import ProfileForm
-from .models import Message
-from .models import Profile
+from .forms import ProfileForm, MessageForm
+from .models import Message, Profile
+
 
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('id','external_id', 'name')
+    list_display = (
+        'id','external_id', 'name','first_name','last_name'
+
+    )
     form = ProfileForm
 
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('id','profile','text','created_at')
+    list_display = ('id','profile','order_status','text','created_at')
+    list_editable = ['order_status']
+
+    form = MessageForm
+
+
+
+
 
 admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Message, MessageAdmin)
